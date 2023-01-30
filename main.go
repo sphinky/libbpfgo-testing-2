d55f9f98fdfb42863b328991a003846419140404
Muhammad <muhammad.abdel.halim@oracle.com>
2023-01-30 23:25:40 +0000

    ...

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
-
 import "C"
 
 import (
@@ -31,12 +30,12 @@ func main() {
 		os.Exit(-1)
 	}
 
-	prog2, err := bpfModule.GetProgram("kprobe__vfs_rename")
+	prog, err := bpfModule.GetProgram("kprobe__vfs_write")
 	if err != nil {
 		os.Exit(-1)
 	}
 
-	_, err = prog2.AttachKprobe("vfs_rename")
+	_, err = prog.AttachKprobe("vfs_write")
 	if err != nil {
 		os.Exit(-1)
 	}
@@ -53,14 +52,10 @@ func main() {
 		event := <-eventsChannel
 		pid := int(binary.LittleEndian.Uint32(event[0:4])) // Treat first 4 bytes as LittleEndian Uint32
 		uid := int(binary.LittleEndian.Uint32(event[4:8])) // Remove excess 0's from comm, treat as string
-		comm := string(bytes.TrimRight(event[8:100], "\x00")) // Remove excess 0's from comm, treat as string
-		msg := string(bytes.TrimRight(event[100:], "\x00")) // Remove excess 0's from comm, treat as string
-		fmt.Printf("%d, \t %d, \t %v: \t %v\n", pid, uid, comm, msg)
-
+		comm := string(bytes.TrimRight(event[8:], "\x00")) // Remove excess 0's from comm, treat as string
+		fmt.Printf("%d, %d, %v: %v\n", pid, uid, comm, "this process and user are attempting to create a new user namespace")
 	}
 
 	rb.Stop()
 	rb.Close()
 }
-
-func oci_logging()
