64a0bf8d48bcfb36084abe62c0e329b47ccf8509
Muhammad <muhammad.abdel.halim@oracle.com>
2023-02-01 21:26:47 +0000

    latest

--- main.go
+++ main.go
@@ -9,7 +9,21 @@ import (
 	"os"
 
 	bpf "github.com/aquasecurity/tracee/libbpfgo"
+
+	"context"
+	"strconv"
+
+	"github.com/oracle/oci-go-sdk/v36/common"
+	"github.com/oracle/oci-go-sdk/v36/example/helpers"
+	"github.com/oracle/oci-go-sdk/v36/streaming"
 )
+
+type ebpfEvent struct {
+	pid int
+	uid  int
+	pname string
+	msg string
+}
 
 func main() {
 
@@ -52,7 +66,8 @@ func main() {
 
 	rb.Start()
 
-	fmt.Printf("----------------------------------------------------------\n");
+	//fmt.Printf("----------------------------------------------------------\n");
+	//fmt.Printf("| %d \t| %d \t| %v \t| %v \t|\n", "PID", "UID", "Name", "MSG"");
 
 	for {
 		event := <-eventsChannel
@@ -63,8 +78,10 @@ func main() {
 		//process name
 		comm := string(bytes.TrimRight(event[8:200], "\x00")) // Remove excess 0's from comm, treat as string
 		msg := string(bytes.TrimRight(event[200:], "\x00")) // Remove excess 0's from comm, treat as string
-	    //fmt.Printf("|\t\t %d \t\t| %d \t\t| %v \t\t| %v \t\t|\n", "PID", "UID", "Name", "MSG"")
-		fmt.Printf("|%d \t| %d \t| %v \t| %v \t|\n", pid, uid, comm, msg)
+	   
+		event := ebpfEvent{pid := pid,uid:= uid,comm :=comm,msg:=msg);
+		//fmt.Printf("|%d \t| %d \t| %v \t| %v \t|\n", pid, uid, comm, msg);
+		putMsgInStream(ociMessageEndpoint, ociStreamOcid, event);
 	}
 
 	rb.Stop()
@@ -73,4 +90,42 @@ func main() {
 
 func USE(x interface{}) {
 	
+}
+
+const ociMessageEndpoint = "<stream_message_endpoint>"
+const ociStreamOcid = "<stream_OCID>"
+const ociConfigFilePath = "<config_file_path>"
+const ociProfileName = "<config_file_profile_name>"
+
+
+func putMsgInStream(streamEndpoint string, streamOcid string, event ebpfEvent) {
+	fmt.Println("Stream endpoint for put msg api is: " + streamEndpoint)
+
+	provider, err := common.ConfigurationProviderFromFileWithProfile(ociConfigFilePath, ociProfileName, "")
+	helpers.FatalIfError(err)
+
+	streamClient, err := streaming.NewStreamClientWithConfigurationProvider(provider, streamEndpoint)
+	helpers.FatalIfError(err)
+
+	// Create a request and dependent object(s).
+
+	putMsgReq := streaming.PutMessagesRequest{
+		StreamId: common.String(streamOcid),
+		PutMessagesDetails: streaming.PutMessagesDetails{
+			
+			Messages: []streaming.PutMessagesDetailsEntry{
+				{
+					Key: []byte("key dummy-0-" + strconv.Itoa(i)),
+					Value: []byte(event.pid+"|"+event.uid+"|"+event.comm+"|"+event.msg);
+				}
+			}
+		},
+	}
+
+	// Send the request using the service client
+	putMsgResp, err := streamClient.PutMessages(context.Background(), putMsgReq)
+	helpers.FatalIfError(err)
+
+	// Retrieve value from the response.
+	fmt.Println(putMsgResp)
 }
