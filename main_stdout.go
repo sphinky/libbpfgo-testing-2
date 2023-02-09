05d791941ffa521b97e5c61d8ff2301d756de723
Muhammad <muhammad.abdel.halim@oracle.com>
2023-02-09 17:00:03 +0000

    latest

--- main_stdout.go
+++ main_stdout.go
@@ -37,8 +37,9 @@ func main() {
 
 	rb.Start()
 
-	fmt.Printf("----------------------------------------------------------\n")
-	fmt.Printf("| %-10v \t| %-10v \t| %-20s \t| %-30s |\n", "PID", "UID", "Name", "MSG")
+	fmt.Printf("-----------------------------------------------------------------------------\n")
+	fmt.Printf("| %-10v \t| %-10v \t| %-20s \t| %-30s\n", "PID", "UID", "Name", "MSG")
+	fmt.Printf("-----------------------------------------------------------------------------\n")
 	//		   "| %-10d \t| %-10d \t| %-20s \t| %-30s \t|\n"
 	for {
 		event := <-eventsChannel
@@ -52,7 +53,7 @@ func main() {
 		// Remove excess 0's from comm, treat as string
 		msg := string(bytes.TrimRight(event[200:400], "\x00"))
 
-		fmt.Printf("| %-10d \t| %-10d \t| %-20s \t| %-30s |\n", pid, uid, comm, msg)
+		fmt.Printf("| %-10d \t| %-10d \t| %-20s \t| %-30s\n", pid, uid, comm, msg)
 	}
 
 	rb.Stop()
